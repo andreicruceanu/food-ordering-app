174286bd77a9cf0b94488c1b390a1a4e07f067ea
Cruceanu Andrei <cruceanuandrei10@gmail.com>
2024-08-16 15:50:34 +0300

    Auth

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRouter from "./AppRouter.tsx";
 import "./global.css";
+import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Router>
-      <AppRouter />
+      <Auth0ProviderWithNavigate>
+        <AppRouter />
+      </Auth0ProviderWithNavigate>
     </Router>
   </StrictMode>
 );
