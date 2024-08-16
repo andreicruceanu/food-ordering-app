8956743e6d0d2b0dbdacb809c59896e563751c7f
Cruceanu Andrei <cruceanuandrei10@gmail.com>
2024-08-16 13:23:27 +0300

    App router

---
+++ src/AppRouter.tsx
@@ -0,0 +1,14 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Layout from "./layouts/layout";
+
+const AppRouter = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout>Home Page</Layout>} />
+      <Route path="/user-profile" element={<span>USER PROFILE PAGE</span>} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+};
+
+export default AppRouter;
